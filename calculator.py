137b3636dc39f663b942d3c0a97ac8a3ff87021c
Camila Montás <camilamontasg0310@gmail.com>
2026-01-28 10:12:28 -0400

    Add multiplication and division

--- calculator.py
+++ calculator.py
@@ -19,6 +19,10 @@ def main(page: ft.Page):
                 res = a + b
             elif op == "-":
                 res = a - b
+            elif op == "x":
+                res = a * b
+            elif op == "dividir":
+                res = a / b if b != 0 else "∞"
 
             basic_result.value = f"Respuesta: {res}"
 
@@ -30,13 +34,21 @@ def main(page: ft.Page):
     basic_ops = ft.Row([
         ft.ElevatedButton("+", data="+", on_click=calculate_basic),
         ft.ElevatedButton("-", data="-", on_click=calculate_basic),
-    ])
-
-    page.add(
-        num1,
-        num2,
-        basic_ops,
-        basic_result
+        ft.ElevatedButton("x", data="x", on_click=calculate_basic),
+        ft.ElevatedButton("dividir", data="dividir", on_click=calculate_basic),
+        ],
+        spacing=10
     )
+
+    basic_tab = ft.Column(
+        [
+            ft.Row([num1, num2], spacing=10),
+            basic_ops,
+            basic_result
+        ],
+        spacing=20
+    )
+
+    page.add(basic_tab)
 
 ft.app(target=main)
